c63466241c1dcc40ffa4307f1652e3da9de6fd20
Engineer9736 <richard@rscheib.eu>
2021-01-07 23:46:04 +0100

    More fixes.

--- TheEndProtector/src/me/Engineer9736/TheEndProtector/Main.java
+++ TheEndProtector/src/me/Engineer9736/TheEndProtector/Main.java
@@ -204,7 +204,7 @@ public class Main extends JavaPlugin implements Listener {
 		}
 		
 		debugMessage("BlockEvent -> shouldBlockEventBeCancelled: BlockEvent cancelled.");
-		p.sendMessage(ChatColor.RED + "As long as the Ender Dragon is not alive, you can only place End Crystals on the Exit Portal to spawn the Ender Dragon.");
+		p.sendMessage(ChatColor.RED + "Cannot adjust blocks on the main island as the Ender Dragon is not alive.");
 		return true;
 	}
 	
@@ -312,40 +312,12 @@ public class Main extends JavaPlugin implements Listener {
 
 		Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
 			public void run() {
-				try {
-					CoreProtectAPI CoreProtect = getCoreProtect();
-					if (CoreProtect != null) { // Ensure we have access to the API
-						// Rollback one hour, 150 blocks radius from the middle of The End.
-						World theEnd = Bukkit.getServer().getWorld("world_the_end");
-						CoreProtect.performRollback(3600, null, null, null, null, null, 150, new Location(theEnd, 00, 80, 0));
-					}
-				}
-				catch (Exception e) {
-					e.printStackTrace();
-				}
+				Runnable runnable = new Rollback();
+				Thread thread = new Thread(runnable);
+				thread.start();
 			}
 		}, 100L); // Run after 5 seconds.
 	}
 	
-	private CoreProtectAPI getCoreProtect() {
-        Plugin plugin = Bukkit.getServer().getPluginManager().getPlugin("CoreProtect");
-     
-        // Check that CoreProtect is loaded
-        if (plugin == null || !(plugin instanceof CoreProtect)) {
-            return null;
-        }
-
-        // Check that the API is enabled
-        CoreProtectAPI CoreProtect = ((CoreProtect) plugin).getAPI();
-        if (CoreProtect.isEnabled() == false) {
-            return null;
-        }
-
-        // Check that a compatible version of the API is loaded
-        //if (CoreProtect.APIVersion() < 6) {
-       //     return null;
-        //}
-
-        return CoreProtect;
-	}
+	
 }
---
+++ TheEndProtector/src/me/Engineer9736/TheEndProtector/Rollback.java
@@ -0,0 +1,50 @@
+package me.Engineer9736.TheEndProtector;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.plugin.Plugin;
+
+import net.coreprotect.CoreProtect;
+import net.coreprotect.CoreProtectAPI;
+
+public class Rollback implements Runnable {
+	@Override
+	public void run() {
+		try {
+			CoreProtectAPI CoreProtect = getCoreProtect();
+			if (CoreProtect != null) { // Ensure we have access to the API
+				// Rollback one hour, 150 blocks radius from the middle of The End.
+				World theEnd = Bukkit.getServer().getWorld("world_the_end");
+				CoreProtect.performRollback(3600, null, null, null, null, null, 150, new Location(theEnd, 00, 80, 0));
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private CoreProtectAPI getCoreProtect() {
+		
+		
+        Plugin plugin = Bukkit.getServer().getPluginManager().getPlugin("CoreProtect");
+     
+        // Check that CoreProtect is loaded
+        if (plugin == null || !(plugin instanceof CoreProtect)) {
+            return null;
+        }
+
+        // Check that the API is enabled
+        CoreProtectAPI CoreProtect = ((CoreProtect) plugin).getAPI();
+        if (CoreProtect.isEnabled() == false) {
+            return null;
+        }
+
+        // Check that a compatible version of the API is loaded
+        //if (CoreProtect.APIVersion() < 6) {
+       //     return null;
+        //}
+
+        return CoreProtect;
+	}
+}
